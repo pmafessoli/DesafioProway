80eddf8ec693696edfb51c8fbb163e8108406fac
Nathan Kruger <nathan.kruger@senior.com.br>
2022-05-08 21:28:19 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = new List<int>();
+
+            for (int i = 1; i < 1000000; i++)
+            {
+                if (!i.ToString().StartsWith("0"))
+                {
+                    int entradaInvertida = Inverter(i);
+                    if (entradaInvertida != -1)
+                    {
+                        var soma = entradaInvertida + i;
+                        if (soma % 2 != 0)
+                        {
+                            lista.Add(i);
+                            Console.WriteLine(i);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Contador total:" + lista.Count);
+        }
+
+        public static int Inverter(int entrada)
+        {
+            string valor = entrada.ToString();
+            string entradaInvertidatxt = "";
+            for (int j = valor.Length; j > 0; j--)
+            {
+                entradaInvertidatxt += valor[j - 1];
+            }
+            if (entradaInvertidatxt.StartsWith("0"))
+                return -1;
+            return Convert.ToInt32(entradaInvertidatxt);
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o limite de alunos atrasados");
+            int limiteAlunsoAtrasados = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite total de alunos na aula");
+            int totalAlunos = Convert.ToInt32(Console.ReadLine());
+            var alunos = new List<string>(totalAlunos);
+            var cont = 0;
+            for (int i = 0; i <= totalAlunos - 1; i++)
+            {
+                Console.WriteLine($"Digite o tempo de chegada do aluno {i + 1}");
+                alunos.Add(Console.ReadLine());
+                if (Convert.ToInt32(alunos[i]) > 0)
+                    cont++;
+            }
+            if (cont > limiteAlunsoAtrasados)
+                Console.WriteLine($"Aula cancelada devido ao excesso de alunos ausentes.");
+            else
+                Console.WriteLine($"A aula ocorrerá normalmente pois o número de alunos atrasados não ultrapassou o limite.");
+        }
+
+
+
+    }
+}
